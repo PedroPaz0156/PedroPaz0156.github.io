98862ff366ad490b466e1a192e9c0841d0618461
Pedro Paz <n00b56.ppz@gmail.com>
2023-11-28 17:27:25 -0300

    teste popup

--- Instrucoes/JS/index.js
+++ Instrucoes/JS/index.js
@@ -1,1 +1,24 @@
-document.getElementById()
+function solPopUp() {
+    var popup = document.getElementById("myPopUp1");
+    popup.classList.toggle("show");
+  }
+  
+  function lucyPopUp() {
+    var popup = document.getElementById("myPopUp2");
+    popup.classList.toggle("show");
+  }
+
+  function borisPopUp() {
+    var popup = document.getElementById("myPopUp3");
+    popup.classList.toggle("show");
+  }
+
+  function sarmentiPopUp() {
+    var popup = document.getElementById("myPopUp4");
+    popup.classList.toggle("show");
+  }
+
+  function evyePopUp() {
+    var popup = document.getElementById("myPopUp5");
+    popup.classList.toggle("show");
+  }
